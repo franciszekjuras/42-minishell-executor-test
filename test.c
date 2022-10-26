ab9b6498fc0ac840330d623c90e6825583735e3e
fjuras <fjuras@student.42wolfsburg.de>
2022-10-26 22:25:03 +0200

    Add redirections test

--- test.c
+++ test.c
@@ -6,9 +6,13 @@
 /*   By: fjuras <fjuras@student.42wolfsburg.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 18:28:02 by fjuras            #+#    #+#             */
-/*   Updated: 2022/10/24 18:30:22 by fjuras           ###   ########.fr       */
+/*   Updated: 2022/10/26 22:16:18 by fjuras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/*
+#C -- # commands
+*/
 
 #include <stdlib.h>
 #include <string.h>
@@ -16,6 +20,8 @@
 #include <interface/line.h>
 #include <interface/test_line.h>
 #include <stdio.h>
+
+#define GREP "/usr/bin/grep"
 
 int	test_single_cmd_no_args(void)
 {
@@ -26,10 +32,10 @@ int	test_single_cmd_no_args(void)
 	int		stdout_fd;
 	int		file_match;
 
-	TEST_START();
+	TEST_START_CLEAN();
 	i = 0;
 	test_line_init(&line, 1);
-	test_prog_args(&line.progs[i], 1, "./megaphone");
+	test_prog_args(&line.progs[i], "./megaphone", NULL);
 	test_prog_redirs(&line.progs[i++], NULL, NULL);
 	test_line_end(&line, i);
 	stdout_fd = test_redirect_stdout("out/stdout.txt");
@@ -49,10 +55,10 @@ int	test_single_cmd_one_arg(void)
 	int		stdout_fd;
 	int		file_match;
 
-	TEST_START();
+	TEST_START_CLEAN();
 	i = 0;
 	test_line_init(&line, 1);
-	test_prog_args(&line.progs[i], 2, "./megaphone", "hello");
+	test_prog_args(&line.progs[i], "./megaphone", "hello", NULL);
 	test_prog_redirs(&line.progs[i++], NULL, NULL);
 	test_line_end(&line, i);
 	stdout_fd = test_redirect_stdout("out/stdout.txt");
@@ -63,9 +69,34 @@ int	test_single_cmd_one_arg(void)
 	return (TEST_END(res && file_match));
 }
 
+int	test_1C_in_and_out_redir(void)
+{
+	t_line	line;
+	int		i;
+	int		retval;
+	int		res;
+	int		stdout_fd;
+	int		file_match;
+
+	TEST_START_CLEAN();
+	i = 0;
+	test_line_init(&line, 1);
+	test_prog_args(&line.progs[i], GREP, "dog", NULL);
+	test_prog_redirs(&line.progs[i++], "in/animals.txt", "out/out.txt");
+	test_line_end(&line, i);
+	stdout_fd = test_redirect_stdout("out/stdout.txt");
+	retval = minish_execute(line);
+	test_restore_stdout(stdout_fd);
+	file_match = test_expect_file_content("out/stdout.txt", NULL)
+		& test_expect_file_content("out/out.txt", "dog", NULL);
+	res = test_expect_retval(retval, 0);
+	return (TEST_END(res && file_match));
+}
+
 int (*const	g_test_functions[])() = {
 	test_single_cmd_no_args,
 	test_single_cmd_one_arg,
+	test_1C_in_and_out_redir,
 	NULL
 };
 
